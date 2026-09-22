81abc4bbb339c5d73b33d45b4787ea061faf8225
steven7768 <rjynw2yyjn@privaterelay.appleid.com>
2026-09-22 10:33:00 -0700

    Update FoodTruck.java

--- FoodTruck.java
+++ FoodTruck.java
@@ -1,5 +1,6 @@
 public class FoodTruck {
 
+    // constants
     public static final String DEFAULT_NAME = "The Rolling Taco";
     public static final double DEFAULT_MONEY = 100.00;
     public static final double INGREDIENT_COST = 2.50;   // per kit
@@ -22,7 +23,7 @@ public class FoodTruck {
         this(DEFAULT_NAME, DEFAULT_MONEY);
     }
 
-    // name
+    // just a name
     public FoodTruck(String name) {
         this(name, DEFAULT_MONEY);
     }
@@ -39,5 +40,92 @@ public class FoodTruck {
         this.totalRevenue = 0.0;
     }
 
-    // need to mak e buy, sell, spoil, pay, bankrupt
+    // getters
+    public String getName() {
+        return name;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public int getIngredients() {
+        return ingredients;
+    }
+
+    public double getMealPrice() {
+        return mealPrice;
+    }
+
+    public double getReputation() {
+        return reputation;
+    }
+
+    public int getTotalMealsSold() {
+        return totalMealsSold;
+    }
+
+    public void setMealPrice(double newPrice) {
+        if (newPrice > 0) {
+            mealPrice = newPrice;
+        }
+    }
+
+    // clamp to 1-5
+    public void setReputation(double newReputation) {
+        if (newReputation > MAX_REPUTATION) {
+            reputation = MAX_REPUTATION;
+        } else if (newReputation < MIN_REPUTATION) {
+            reputation = MIN_REPUTATION;
+        } else {
+            reputation = newReputation;
+        }
+    }
+
+    // stuff the truck does
+    public boolean canAfford(double cost) {
+        return money >= cost;
+    }
+
+    public boolean buyIngredients(int amount) {
+        double totalCost = amount * INGREDIENT_COST;
+        boolean fitsInFridge = ingredients + amount <= FRIDGE_CAPACITY;
+
+        // need all 3
+        if (amount > 0 && canAfford(totalCost) && fitsInFridge) {
+            money -= totalCost;
+            ingredients += amount;
+            return true;
+        }
+        return false;
+    }
+
+    public int sellMeals(int customers) {
+        int mealsSold = customers;
+
+        double earnings = mealsSold * mealPrice;
+
+        ingredients -= mealsSold;
+        money += earnings;
+        totalRevenue += earnings;
+        totalMealsSold += mealsSold;
+
+        return mealsSold;
+    }
+
+    public int spoilIngredients(double spoilRate) {
+        //  cuts the decimal
+        int spoiled = (int) (ingredients * spoilRate);
+        ingredients -= spoiled;
+        return spoiled;
+    }
+
+    public void payExpense(double amount) {
+        money -= amount;
+    }
+
+    // broke = no kits and can't buy even 1
+    public boolean isBankrupt() {
+        return ingredients == 0 && !canAfford(INGREDIENT_COST);
+    }
 }
